e10b66368c47f117a309b09d3bf749fd0b5e7464
Pauline <chicoyepauline@gmail.com>
2020-05-09 23:30:35 -0400

    Added menu question

--- ATM/src/main/java/ATM/ATM.java
+++ ATM/src/main/java/ATM/ATM.java
@@ -1,12 +1,17 @@
 package ATM;
 
+import java.util.Scanner;
+
 /**
- *
  * @author Pauline Chicoye
  */
 public class ATM {
     public static void main(String[] args) {
+        System.out.println("Hello. Welcome to the virtual ATM machine");
+        System.out.println("Please input your full name");
         
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
     }
     
 }
