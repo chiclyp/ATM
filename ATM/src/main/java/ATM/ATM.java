b7695dd6e4300aaea9bd3eafc1877acfdf5cdef9
Pauline <chicoyepauline@gmail.com>
2020-05-10 00:23:43 -0400

    Added switch case of menu

--- ATM/src/main/java/ATM/ATM.java
+++ ATM/src/main/java/ATM/ATM.java
@@ -7,11 +7,56 @@ import java.util.Scanner;
  */
 public class ATM {
     public static void main(String[] args) {
-        System.out.println("Hello. Welcome to the virtual ATM machine");
+        Account details = new Account(0);
+        details.setBalance(2500.00);
+        System.out.println("Hello. This is the virtual ATM machine");
         System.out.println("Please input your full name");
         
         Scanner scan = new Scanner(System.in);
         String name = scan.nextLine();
+       
+        System.out.println("Welcome "+ name + ".");
+        System.out.println("Please input your 4 digit nip.");
+        int nip = scan.nextInt();
+        
+        while (nip != details.nip()){
+            System.out.println(details.incorrectNip());
+            nip = scan.nextInt();
+        }
+        
+        if (nip == details.nip()){
+            System.out.println(details.correctNip());
+        }
+        
+        System.out.println("Please input the number of option you would "
+                + "like to select");
+        System.out.println("(1) Deposit");
+        System.out.println("(2) Withdrawal");
+        System.out.println("(3) See Balance");
+        
+        int choice = scan.nextInt();
+        
+        switch (choice){
+            case 1:
+                System.out.println("What will be the amount deposited today?");
+                double deposit = scan.nextDouble();
+                System.out.println("Are you sure you want " + deposit + 
+                        "deposited in your account?");
+
+                break;
+            case 2:
+                System.out.println("What will be the amount withdrawled today?");
+                double withdrawal = scan.nextDouble();
+                System.out.println("Are you sure you want " + withdrawal + 
+                        "withdrawled in your account?");
+                break;
+            case 3:
+                System.out.println(details.balance()+"$");
+                break;   
+        }
+        
+      
+        
     }
     
 }
