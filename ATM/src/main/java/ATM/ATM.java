3050a766ce7922276fb98bd9db53bcfc7d131064
Pauline <chicoyepauline@gmail.com>
2020-05-12 00:19:30 -0400

    Finished the general aspect of the assignment

--- ATM/src/main/java/ATM/ATM.java
+++ ATM/src/main/java/ATM/ATM.java
@@ -87,17 +87,50 @@ public class ATM {
                 
                 if (answer.equalsIgnoreCase(yes)){
                     details.setWithSum(withdrawal);
+                    System.out.println("Here are your Withdraw Details:");
+                    System.out.println("Number of 100 bills received: " + details.toHundBills());
+                    System.out.println("Number of 50 bills received: " + details.toFiftyBills());
+                    System.out.println("Number of 20 bills received: " + details.toTwentyBills());
+                    System.out.println("Number of 10 bills received: " + details.toTenBills());
+                    System.out.println("Number of 5 bills received: " + details.toFiveBills());
                     System.out.println(details.currWith());
-                   
                 } 
                 break;
                 
             case 3:
                 System.out.println(details.balance()+"$");
-                break;   
+                break;
         }
+        int i;
+        System.out.println("Your transaction has been made");
+        System.out.println("-------------------------------------------------------------------------------");
+        for(i = 0; i < 2; i++){
+            System.out.println("Here is your random quote of the day");
+            System.out.println("\"Nothing in the universe can stop you from letting go and stating over\"");
+            i++;
+        }
+        String at = "@";
+        String emYes = "yes";
+       
         
-      
+        System.out.println("Would you like the receipt to be sent to your email?");
+        String ans = scan.next();
+        
+        if(ans.equalsIgnoreCase(emYes)){
+            System.out.println("Please input your email");
+            String email = scan.next();
+            while(!email.contains(at)){
+                System.out.println("Invalid Email. Try Again");
+                email = scan.next();
+            }
+            
+            System.out.println("The email has been sent to " + email);
+            }
+        
+        
+            System.out.println("Have a nice day. Goodbye!");
+            System.out.println(dtf.format(now));
+            
         
     }
     
